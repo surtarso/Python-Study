f8866f458ea7397db6f7da2d1fdab6a1643e786c
surtarso <tarso.galvao@gmail.com>
2022-06-12 17:18:50 -0300

    added stockpicker from yahoo-fin for ibovespa

--- Learning Django/Stock Tracker App/stockproject/mainapp/views.py
+++ Learning Django/Stock Tracker App/stockproject/mainapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from yahoo_fin.stock_info import *
+
 
 # Create your views here.
 
+## adds the stockpicker multi-click ui
 def stockPicker(request):
-    return render(request, 'mainapp/stockpicker.html')
+    stock_picker = tickers_ibovespa()
+    print(stock_picker)
+    return render(request, 'mainapp/stockpicker.html', {'stockpicker':stock_picker})
 
+## adds the table of stocks
 def stockTracker(request):
     return render(request, 'mainapp/stocktracker.html')
