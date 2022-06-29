413734ad161289174a62b5863c5a56fe225a1f85
surtarso <tarso.galvao@gmail.com>
2022-06-29 19:58:34 -0300

    trying cron

--- Desafio Alpha - StocksWatch/StocksWatch/stockproject/mainapp/cron.py
+++ Desafio Alpha - StocksWatch/StocksWatch/stockproject/mainapp/cron.py
@@ -1,13 +1,12 @@
 import queue
 from threading import Thread
 from time import sleep
-from .models import Alerta
+from mainapp.models import Alerta
 import yfinance as yf
 from django.core.mail import send_mail
-import schedule
 
 
-###--------------------------- TENTATIVA 1 -------------------------------------
+
 ##                 str    str    float  float     int     int
 ## novo_alerta = [email, ativo, compra, venda, periodo, duracao]
 ##                  0      1      2       3       4        5 
@@ -44,9 +43,10 @@ def checaPreco(novo_alerta):
 
 
 
-def pegaAlertas():  ##funcionando
+def pegaAlertas():
     
     novo_alerta = []
+    todos_os_alertas = []
     # todos os alertas:
     alertas = Alerta.objects.all()
 
@@ -60,82 +60,24 @@ def pegaAlertas():  ##funcionando
         duracao = i.duracao
 
         novo_alerta = [email, ativo, compra, venda, periodo, duracao]
-
-        checaPreco(novo_alerta)
+        todos_os_alertas.append(novo_alerta)
     
+    # multithread todos_os_alertas
+    n_threads = len(todos_os_alertas)
+    thread_list = []
+    que = queue.Queue()
 
+    for i in range(n_threads):
+        thread = Thread(
+            target = lambda q,
+            arg1: q.put({todos_os_alertas[i]: checaPreco(arg1)}),
+            args = (que, todos_os_alertas[i])
+            )
+        thread_list.append(thread)
+        thread_list[i].start()
 
-
-
-
-
-
-# ###--------------------------- TENTATIVA 2 -------------------------------------
-
-
-# def checaPreco_teste2(novo_alerta):
-
-#     msg_venda = ("{} - preço de VENDA R${} foi atingido.").format(novo_alerta[1], novo_alerta[3]) #ticker, venda
-#     msg_compra = ("{} - preço de COMPRA R${} foi atingido.").format(novo_alerta[1], novo_alerta[2]) #ticker, compra
-#     msg_fim = ('Sua operação com {} terminou!').format(novo_alerta[1]) #ticker
-
-#     ticker = yf.Ticker(novo_alerta[1]+".SA") #ativo
-
-#     cotacao = round(ticker.info['regularMarketPrice'], 2)
+    for thread in thread_list:
+        thread.join()
     
-#     if cotacao <= float(novo_alerta[2]): #compra
-#         # send email compra
-#         send_mail('StockWatch Alerta!', msg_compra,'admin@stockwatch.com',
-#                         [novo_alerta[0]], fail_silently=False,) #email
-
-#     elif cotacao >= float(novo_alerta[3]): #venda
-#         # send email venda
-#         send_mail('StockWatch Alerta!', msg_venda,'admin@stockwatch.com',
-#                         [novo_alerta[0]], fail_silently=False,) #email
-
-#     else:
-#         pass
-    
-#     schedule.every(novo_alerta[5]).minutes.do(checaPreco_teste2)
-
-#     while True:
-#         schedule.run_pending()
-#         sleep(novo_alerta[4]) #periodo
-
-
-# def pegaAlertas_teste2():  ## nao testado
-    
-#     novo_alerta = {}
-#     # todos os alertas:
-#     alertas = Alerta.objects.all()
-    
-#     #teste multithreading
-#     n_threads = len(alertas)
-#     thread_list = []
-#     que = queue.Queue()
-
-#     # pega de 1 em 1
-#     for i in range(n_threads):
-#         thread = Thread(
-#             target = checaPreco_teste2,
-#             args = (
-#                 que,
-#                 alertas[i].email,
-#                 alertas[i].ativo.ticker,
-#                 alertas[i].compra,
-#                 alertas[i].venda,
-#                 alertas[i].periodo,
-#                 alertas[i].duracao
-#             )
-#         )
-#         thread_list.append(thread)
-#         thread_list[i].start()
-
-#     for thread in thread_list:
-#         thread.join()
-
-#     while not que.empty():
-#         result = que.get()
-#         novo_alerta.update(result)
-
-#     checaPreco_teste2(novo_alerta)
+    while not que.empty():
+        que.get()
