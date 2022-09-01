cccfc7c546623f80dc5da5034c2f0d343c9bcf6f
surtarso <tarso.galvao@gmail.com>
2022-09-01 10:41:49 -0300

    moved stonks to own repo

--- Desafio Alpha - StocksWatch/StocksWatch/stockproject/mainapp/views/views_home.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-## -------------------------------------------------------HOME:
-def home(request):
-    return render(request, 'mainapp/home.html')
-
-def http_404_error(request, exception):
-    return render(request, 'mainapp/404.html', status=404)
